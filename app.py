d7da1160fe650168ea98657e567241a37a524627
Mattral <88831350+Mattral@users.noreply.github.com>
2023-10-25 23:31:47 +0630

    Update app.py

--- app.py
+++ app.py
@@ -1,40 +1,27 @@
 import streamlit as st
 import random
 
-# Set page title and icon
-st.set_page_config(page_title="Guess the Number", page_icon="🔍")
-
-# Page title and description
-st.title("Guess the Number Game")
-st.write("I'm thinking of a number between 1 and 100. Can you guess it?")
-
-# Generate a random number between 1 and 100
+# Generate a random secret number when the game starts
 secret_number = random.randint(1, 100)
 
-# Initialize game state
+st.title("Guess the Number Game")
+
+st.write("I'm thinking of a number between 1 and 100. Can you guess what it is?")
+
 attempts = 0
-feedback = ""
 
-form = st.form(key='my_form')
-user_guess = form.number_input("Enter your guess:", min_value=1, max_value=100)
-submitted = form.form_submit_button("Check")
-
-if submitted:
+# Main game loop
+while True:
+    user_guess = st.number_input("Enter your guess:", min_value=1, max_value=100, key="guess")
+    
     attempts += 1
-    if user_guess < secret_number:
-        feedback = "Try a higher number."
-    elif user_guess > secret_number:
-        feedback = "Try a lower number."
+    
+    if user_guess == secret_number:
+        st.write(f"Congratulations! You guessed the correct number {secret_number} in {attempts} attempts.")
+        break
+    elif user_guess < secret_number:
+        st.write("Try a higher number.")
     else:
-        feedback = f"Congratulations! You guessed the number {secret_number} in {attempts} attempts."
-        secret_number = random.randint(1, 100)  # Reset the secret number
-        attempts = 0
+        st.write("Try a lower number")
 
-# Display feedback
-st.write(feedback)
-
-# Display attempts
-st.write(f"Attempts: {attempts}")
-
-# Hint for a new game
-st.write("Can you guess the new number?")
+st.write("Thanks for playing!")
