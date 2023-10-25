fa0f1044034dca3f4e0fa80384cfbf460c372b0e
Mattral <88831350+Mattral@users.noreply.github.com>
2023-10-25 23:16:44 +0630

    Update app.py

--- app.py
+++ app.py
@@ -11,30 +11,31 @@ st.write("I'm thinking of a number between 1 and 100. Can you guess it?")
 # Generate a random number between 1 and 100
 secret_number = random.randint(1, 100)
 
-# Input field for the user's guess
-user_guess = st.number_input("Enter your guess:", min_value=1, max_value=100)
-
 # Initialize game state
 attempts = 0
 feedback = ""
 
-if st.button("Check"):
-    attempts += 1
-    if user_guess < secret_number:
-        feedback = "Try a higher number."
-    elif user_guess > secret_number:
-        feedback = "Try a lower number."
-    else:
-        feedback = f"Congratulations! You guessed the number {secret_number} in {attempts} attempts."
-        secret_number = random.randint(1, 100)  # Reset the secret number
-        attempts = 0
+while True:
+    # Input field for the user's guess
+    user_guess = st.number_input("Enter your guess:", min_value=1, max_value=100)
 
-# Display feedback
-st.write(feedback)
+    if st.button("Check"):
+        attempts += 1
+        if user_guess < secret_number:
+            feedback = "Try a higher number."
+        elif user_guess > secret_number:
+            feedback = "Try a lower number."
+        else:
+            feedback = f"Congratulations! You guessed the number {secret_number} in {attempts} attempts."
+            secret_number = random.randint(1, 100)  # Reset the secret number
+            attempts = 0
+            break
+
+    # Display feedback
+    st.write(feedback)
 
 # Display attempts
 st.write(f"Attempts: {attempts}")
 
 # Hint for a new game
 st.write("Can you guess the new number?")
-
